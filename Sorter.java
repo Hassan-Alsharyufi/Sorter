e19284741cbe32570a2ef2681abbabf009e12cd8
Hassan-Alsharyufi <h.alsharyoufi@gmail.com>
2023-02-10 22:16:01 +0300

    selection sort

--- Sorter.java
+++ Sorter.java
@@ -2,18 +2,18 @@ public class Sorter{
     public static void main(String[] args) {
         
     }
-    public static void selectionSort(int[] arr){  
-        for (int i = 0; i < arr.length - 1; i++)  
+    public static void selectionSort(int[] array){  
+        for (int i = 0; i < array.length - 1; i++)  
         {  
             int index = i;  
-            for (int j = i + 1; j < arr.length; j++){  
-                if (arr[j] < arr[index]){  
+            for (int j = i + 1; j < array.length; j++){  
+                if (array[j] < array[index]){  
                     index = j;//searching for lowest index  
                 }  
             }  
-            int smallerNumber = arr[index];   
-            arr[index] = arr[i];  
-            arr[i] = smallerNumber;  
+            int smallerNumber = array[index];   
+            array[index] = array[i];  
+            array[i] = smallerNumber;  
         }  
     }  
 }
